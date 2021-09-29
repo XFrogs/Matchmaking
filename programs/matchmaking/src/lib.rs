f1219301fc957a407b4a5444f762c5dbe99d2659
DESKTOP-P2VSRRP\tanmay <mrcooldinesh1@gmail.com>
2021-09-29 23:41:15 +0530

    second

--- programs/matchmaking/src/lib.rs
+++ programs/matchmaking/src/lib.rs
@@ -8,10 +8,46 @@ pub mod matchmaking {
     pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
         Ok(())
     }
+
+    //#[access_control(CreateMatch::accounts(&ctx, nonce))]
+    pub fn create_match(ctx: Context<CreateMatch>, amount: u64, nonce: u8) -> ProgramResult {
+        let MatchData = &mut ctx.accounts.matchdata;
+
+        //emmiter here for indexing
+
+        Ok(())
+    }
+    pub fn conclude_match(ctx: Context<ConcludeMatch>) -> ProgramResult {
+        Ok(())
+    }
+    pub fn settle_prize(ctx: Context<SettlePrize>) -> ProgramResult {
+        Ok(())
+    }
 }
 
 #[derive(Accounts)]
 pub struct Initialize {}
 
 #[derive(Accounts)]
-pub struct MatchData {}
+pub struct CreateMatch {
+    #[account(init, payer = player , space= 8+8 )]
+    pub matchdata: ProgramAccount<'info, MatchData>,
+
+    #[account(mut)]
+    pub player_one: AccountInfo<'info>,
+    pub player_two: AccountInfo<'info>,
+}
+
+#[derive(Accounts)]
+pub struct ConcludeMatch {}
+
+#[derive(Accounts)]
+pub struct SettlePrize {}
+
+#[account]
+pub struct MatchData {
+    pub player_one: Pubkey,
+    pub player_two: Pubkey,
+    pub prize_settled: bool,
+    pub winner_address: Pubkey,
+}
--- tests/matchmaking.js
+++ tests/matchmaking.js
@@ -1,14 +1,15 @@
-const anchor = require('@project-serum/anchor');
+const anchor = require("@project-serum/anchor");
 
-describe('matchmaking', () => {
-
+describe("matchmaking", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.Provider.env());
 
-  it('Is initialized!', async () => {
+  it("Is initialized!", async () => {
     // Add your test here.
     const program = anchor.workspace.Matchmaking;
     const tx = await program.rpc.initialize();
     console.log("Your transaction signature", tx);
   });
+
+  it("Creates Match", async () => {});
 });
